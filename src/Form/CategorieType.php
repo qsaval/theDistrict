9c1f7ae8fa5ff06fb114e7ce48a3ba61cd02b2ff
qsaval <quentin.saval@hotmail.fr>
2023-04-12 11:48:22 +0200

    modif

--- src/Form/CategorieType.php
+++ src/Form/CategorieType.php
@@ -4,20 +4,24 @@ namespace App\Form;
 
 use App\Entity\Categorie;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Vich\UploaderBundle\Form\Type\VichImageType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Vich\UploaderBundle\Form\Type\VichFileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class CategorieType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('libelle')
-            ->add('image')
-            ->add('active')
+            ->add('libelle',TextType::class)
+            ->add('image',TextType::class)
+            ->add('active',ChoiceType::class, [
+                'choices' => [
+                    'Yes' => 'Yes',
+                    'No' => 'No'
+                ]
+            ])
         ;
     }
 
--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -6,16 +6,19 @@ use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ContactType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('auteur')
-            ->add('email')
-            ->add('suject')
-            ->add('contenu')
+            ->add('auteur',TextType::class)
+            ->add('email',EmailType::class)
+            ->add('suject',TextType::class)
+            ->add('contenu',TextareaType::class)
         ;
     }
 
